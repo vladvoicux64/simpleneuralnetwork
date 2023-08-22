77fd3266563d56bbb4c7d932a4cf2a63106cf541
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2023-08-22 19:37:30 +0300

    Modified leaky ReLU and added tanh.

--- leakyReLU.py
+++ leakyReLU.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def leaky_relu(x, alpha = 0.01):
-    return np.maximum(alpha * x, x)
+    return np.where(x>0, x, x * alpha)
 
 
 def leaky_relu_derivative(x, alpha = 0.01):
---
+++ tanh.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def tanh(x):
+    return np.tanh(x)
+
+
+def tanh_derivative(x):
+    return 1-np.tanh(x)**2
