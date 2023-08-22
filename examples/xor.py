5fee56c9a7b1e693450927e81f99ce6847939de4
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2023-08-23 01:22:38 +0300

    Cleaned up the repository.

---
+++ examples/xor.py
@@ -0,0 +1,25 @@
+import numpy as np
+import sys
+
+from src.network import Network
+from src.fc_layer import FCLayer
+from src.activation_layer import ActivationLayer
+from src.leakyReLU import leaky_relu, leaky_relu_derivative
+from src.loss import mse, mse_derivative
+
+sys.path.append('../src/')
+
+training_input = np.array([[[0, 0]], [[0, 1]], [[1, 0]], [[1, 1]]])
+training_output = np.array([[[0]], [[1]], [[1]], [[0]]])
+
+net = Network()
+net.add_layer(FCLayer(2, 2, True))
+net.add_layer(ActivationLayer(leaky_relu, leaky_relu_derivative))
+net.add_layer(FCLayer(2, 1, True))
+net.add_layer(ActivationLayer(leaky_relu, leaky_relu_derivative))
+
+net.use_loss(mse, mse_derivative)
+net.train(training_input, training_output, epoch_count=1000, learning_rate=0.1)
+
+out = net.network_forward_propagation(training_input)
+print(out)
