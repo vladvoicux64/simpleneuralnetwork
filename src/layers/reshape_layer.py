381b56922ce2357ab6acfae712140260eaf71503
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2023-09-07 22:10:47 +0300

    Implemented the reshape layer.

---
+++ src/layers/reshape_layer.py
@@ -0,0 +1,16 @@
+import numpy as np
+from layer import Layer
+
+
+class ReshapeLayer(Layer):
+    def __init__(self, input_shape, output_shape):
+        super().__init__()
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+    def forward_propagation(self, input):
+        return np.reshape(input, self.output_shape)
+
+    def backward_propagation(self, output_gradient, learning_rate):
+        return np.reshape(output_gradient, self.input_shape)
+
