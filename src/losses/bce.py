326646cd878bd9e9ff74a171b110d1c394abe560
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2023-09-15 04:07:27 +0300

    Fixed a bug in the BCE loss function.

--- src/losses/bce.py
+++ src/losses/bce.py
@@ -2,8 +2,10 @@ import numpy as np
 
 
 def bce(y_true, y_predicted):
-    return -np.mean(y_true * np.log(y_predicted) + (1 - y_true) * np.log(1 - y_predicted))
+    return -np.mean(y_true * np.log(y_predicted) + (np.ones_like(y_true) - y_true) * np.log(
+        np.ones_like(y_predicted) - y_predicted))
 
 
 def bce_derivative(y_true, y_predicted):
-    return ((1 - y_true) / (1 - y_predicted) - y_true / y_predicted) / np.size(y_true)
+    return ((np.ones_like(y_true) - y_true) / (
+                np.ones_like(y_predicted) - y_predicted) - y_true / y_predicted) / np.size(y_true)
