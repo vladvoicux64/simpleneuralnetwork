5bd4bc786a31b9690f2e2054d6ee990dabceaeac
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2023-09-27 19:16:29 +0300

    Updated MNIST example for better accuracy.

--- examples/mnist.py
+++ examples/mnist.py
@@ -8,8 +8,9 @@ from src.activation_functions.softmax import softmax, softmax_derivative
 from src.layers.activation_layer import ActivationLayer
 from src.layers.convolutional_layer import ConvolutionalLayer
 from src.layers.fc_layer import FCLayer
+from src.layers.maxpooling_layer import MaxpoolingLayer
 from src.layers.reshape_layer import ReshapeLayer
-from src.losses.xce import xce, xce_derivative_softmax
+from src.losses.xentropy import xentropy, xentropy_derivative
 from src.network import Network
 
 
@@ -34,16 +35,22 @@ test_input, test_output = preprocess_data(test_input, test_output, 50)
 
 net = Network()
 net.layers = [
-    ConvolutionalLayer((1, 28, 28), 5, 32, True),
+    ConvolutionalLayer((1, 28, 28), 3, 32, True),
     ActivationLayer(leaky_relu, leaky_relu_derivative),
-    ReshapeLayer((32, 24, 24), (1, 32 * 24 * 24)),
-    FCLayer(32 * 24 * 24, 100, True),
+    MaxpoolingLayer((32, 26, 26), (2, 2)),
+    ConvolutionalLayer((32, 13, 13), 3, 16, True),
+    ActivationLayer(leaky_relu, leaky_relu_derivative),
+    MaxpoolingLayer((16, 11, 11), (2, 2)),
+    ConvolutionalLayer((16, 5, 5), 3, 16, True),
+    ActivationLayer(leaky_relu, leaky_relu_derivative),
+    ReshapeLayer((16, 3, 3), (1, 144)),
+    FCLayer(144, 100, True),
     ActivationLayer(leaky_relu, leaky_relu_derivative),
     FCLayer(100, 2, True),
-    ActivationLayer(softmax, softmax_derivative, using_softmax_xce=True),
+    ActivationLayer(softmax, softmax_derivative),
 ]
 
-net.use_loss(xce, xce_derivative_softmax)
+net.use_loss(xentropy, xentropy_derivative)
 
 net.train_minibatch(training_input, training_output, epoch_count=20, batch_size=1, learning_rate=0.00004)
 
