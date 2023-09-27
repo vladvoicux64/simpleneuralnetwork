948ba23a7c1f6e813880579160d4492ec3ab957e
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2023-09-27 17:19:45 +0300

    Chose a more adequate name for X entropy.

--- src/losses/xentropy.py
+++ src/losses/xentropy.py
@@ -1,9 +1,9 @@
 import autograd.numpy as np
 
 
-def xce(y_true, y_predicted):
+def xentropy(y_true, y_predicted):
     return -np.sum(y_true * np.log(y_predicted + np.ones_like(y_predicted) * 10 ** -100))
 
 
-def xce_derivative(y_true, y_predicted):
+def xentropy_derivative(y_true, y_predicted):
     return -y_true / (y_predicted + np.ones_like(y_predicted) * 10 ** -100)
