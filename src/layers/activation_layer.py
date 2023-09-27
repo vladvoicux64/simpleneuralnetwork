804cea50776cae413234149e73969f8c891d1b9c
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2023-09-27 17:18:17 +0300

    Generalized X entropy loss derivative.

--- src/layers/activation_layer.py
+++ src/layers/activation_layer.py
@@ -5,11 +5,10 @@ from src.layers.layer import Layer
 
 
 class ActivationLayer(Layer):
-    def __init__(self, activation, activation_derivative, using_softmax_xce=False):
+    def __init__(self, activation, activation_derivative):
         super().__init__()
         self.activation = activation
         self.activation_derivative = activation_derivative
-        self.use_simplified_derivative = using_softmax_xce
 
     def forward_propagation(self, input):
         self.input = input
@@ -18,9 +17,6 @@ class ActivationLayer(Layer):
 
     def backward_propagation(self, output_gradient, learning_rate):
         if self.activation == softmax:
-            if self.use_simplified_derivative:
-                return output_gradient
-            else:
-                return np.dot(output_gradient, self.activation_derivative(self.input))
+            return np.dot(output_gradient, self.activation_derivative(self.input).T)
         else:
             return self.activation_derivative(self.input) * output_gradient
--- src/losses/xce.py
+++ src/losses/xce.py
@@ -1,11 +1,9 @@
-import numpy as np
+import autograd.numpy as np
 
 
 def xce(y_true, y_predicted):
-    return -np.mean(y_true * np.log(y_predicted + np.ones_like(y_predicted) * 10 ** -10))
+    return -np.sum(y_true * np.log(y_predicted + np.ones_like(y_predicted) * 10 ** -100))
 
 
-def xce_derivative_softmax(y_true, y_predicted):
-    return y_predicted - y_true
-
-
+def xce_derivative(y_true, y_predicted):
+    return -y_true / (y_predicted + np.ones_like(y_predicted) * 10 ** -100)
